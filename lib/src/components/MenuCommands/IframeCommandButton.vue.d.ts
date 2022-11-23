62c5365c5ad5e8678b84977101faf4dc5731f8af
lihang <lihang1329@gmail.com>
2022-11-23 13:35:11 +0000

    update: ci

---
+++ lib/src/components/MenuCommands/IframeCommandButton.vue.d.ts
@@ -0,0 +1,25 @@
+import { Editor } from '@tiptap/vue-3';
+declare const _sfc_main: import("vue").DefineComponent<{
+    editor: {
+        type: typeof Editor;
+        required: true;
+    };
+}, {
+    t: unknown;
+    enableTooltip: boolean;
+    isCodeViewMode: boolean;
+}, {
+    imageUploadDialogVisible: boolean;
+    uploading: boolean;
+}, {
+    imageNodeOptions(): any;
+}, {
+    openInsertVideoControl(): Promise<void>;
+    uploadImage(requestOptions: any): Promise<void>;
+}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    editor: {
+        type: typeof Editor;
+        required: true;
+    };
+}>>, {}>;
+export default _sfc_main;
