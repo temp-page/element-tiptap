2848cce099a7590f5698b019bd1f3d212ceb9939
lihang <lihang1329@gmail.com>
2022-11-25 00:16:50 +0800

    feat: 修复编辑链接的问题

--- src/components/MenuCommands/Link/EditLinkCommandButton.vue
+++ src/components/MenuCommands/Link/EditLinkCommandButton.vue
@@ -1,45 +1,45 @@
 <template>
   <div>
     <command-button
-      :command="openEditLinkDialog"
-      :enable-tooltip="enableTooltip"
+      :command='openEditLinkDialog'
+      :enable-tooltip='enableTooltip'
       :tooltip="t('editor.extensions.Link.edit.tooltip')"
-      icon="edit"
+      icon='edit'
     />
 
     <el-dialog
       :title="t('editor.extensions.Link.edit.control.title')"
-      :visible.sync="editLinkDialogVisible"
-      :append-to-body="true"
-      width="400px"
-      custom-class="el-tiptap-edit-link-dialog"
+      v-model='editLinkDialogVisible'
+      :append-to-body='true'
+      width='400px'
+      custom-class='el-tiptap-edit-link-dialog'
     >
-      <el-form :model="linkAttrs" label-position="right" size="small">
+      <el-form :model='linkAttrs' label-position='right' size='small'>
         <el-form-item
           :label="t('editor.extensions.Link.edit.control.href')"
-          prop="href"
+          prop='href'
         >
-          <el-input v-model="linkAttrs.href" autocomplete="off" />
+          <el-input v-model='linkAttrs.href' autocomplete='off' />
         </el-form-item>
 
-        <el-form-item prop="openInNewTab">
-          <el-checkbox v-model="linkAttrs.openInNewTab">
+        <el-form-item prop='openInNewTab'>
+          <el-checkbox v-model='linkAttrs.openInNewTab'>
             {{ t('editor.extensions.Link.edit.control.open_in_new_tab') }}
           </el-checkbox>
         </el-form-item>
       </el-form>
 
       <template #footer>
-        <el-button size="small" round @click="closeEditLinkDialog">
+        <el-button size='small' round @click='closeEditLinkDialog'>
           {{ t('editor.extensions.Link.edit.control.cancel') }}
         </el-button>
 
         <el-button
-          type="primary"
-          size="small"
+          type='primary'
+          size='small'
           round
           @mousedown.prevent
-          @click="updateLinkAttrs"
+          @click='updateLinkAttrs'
         >
           {{ t('editor.extensions.Link.edit.control.confirm') }}
         </el-button>
@@ -48,17 +48,10 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import { defineComponent, inject } from 'vue';
 import { Editor } from '@tiptap/vue-3';
-import {
-  ElDialog,
-  ElForm,
-  ElFormItem,
-  ElInput,
-  ElCheckbox,
-  ElButton,
-} from 'element-plus';
+import { ElButton, ElCheckbox, ElDialog, ElForm, ElFormItem, ElInput } from 'element-plus';
 import CommandButton from '../CommandButton.vue';
 
 export default defineComponent({
